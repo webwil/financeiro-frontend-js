8d921cb31774803837e797664f4043d0eca25b29
Willian Garcia <webwil2012@gmail.com>
2023-07-28 17:09:57 -0300

    Criando roteamento do sistema

--- routing.js
+++ routing.js
@@ -1,1 +1,20 @@
 // Arquivo Reponsável pelas rotas da aplicação
+
+const routes = {
+    '/': '<app-login></app-login>',
+    '/index.html': '<app-login></app-login>',
+    '/dashboard': '<app-dashboard></app-dashboard>'
+}
+
+console.log(routes);
+
+const renderRoute = async (route) => {
+    const routeContent = routes[route];
+    document.getElementById('main-page').innerHTML = routeContent;
+}
+
+const navigateTo = (pathname) => {
+    renderRoute(pathname);
+}
+
+renderRoute(window.location.pathname);
