978777a4fe1b30e538b8717d49e4ee460895111e
Willian Garcia <webwil2012@gmail.com>
2023-08-06 15:24:10 -0300

    Exibindo imagem do perfil do usuário

--- src/components/dashboard/menu/Menu.js
+++ src/components/dashboard/menu/Menu.js
@@ -6,7 +6,30 @@ class Menu extends HTMLElement {
         .then(response => response.text())
         .then(text => this.innerHTML = text)
     }
+
+    connectedCallback() {
+        setTimeout(() => {
+            getImageUser();
+        }, 1000)
+    }
+
 }
+
+const getImageUser = () => {
+    const img = document.querySelector('.profile-img');
+
+    const nameImage = JSON.parse(localStorage.getItem('userInfo'));
+
+    window.downloadImage(`${window.apiURL}/download/image`, nameImage.image)
+        .then(response => response.json())
+        .then(response => {
+            console.log(response);
+
+            let url = 'data:image/jpg;base64, ' + response.image;
+            img.src = url;
+        })
+}
+
 if ('customElements' in window) {
     customElements.define('app-menu', Menu);
 }
