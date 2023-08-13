a7fa173a41d01f8b29d8f454360390460afc26ea
Willian Garcia <webwil2012@gmail.com>
2023-08-13 17:00:13 -0300

    Criando selects de categoria da receita e método de pagamento

--- src/components/dashboard/income-list/add-income-modal/AddIcomeModal.js
+++ src/components/dashboard/income-list/add-income-modal/AddIcomeModal.js
@@ -6,6 +6,14 @@ class AddIcomeModal extends HTMLElement {
         .then(response => response.text())
         .then(text => this.innerHTML = text)
     }
+
+    connectedCallback() {
+        setTimeout(() => {
+            createSelectElement('income', 'add-income', '.select-container-income', 'Categoria da Receita', window.typeIncome, 12);
+            createSelectElement('income', 'payment-method', '.select-container-payment-method-income', 'Método de Pagamento', window.paymentMethod, 9);
+        }, 1000)
+    }
+
 }
 if ('customElements' in window) {
     customElements.define('app-add-income-modal', AddIcomeModal);
--- src/utils/GlobalVariables.js
+++ src/utils/GlobalVariables.js
@@ -10,4 +10,73 @@ window.gender = [
     }
 ];
 
+window.typeIncome = [
+    {
+        name: 'Salário'
+    },
+    {
+        name: 'Rendimentos autonomos'
+    },
+    {
+        name: 'Rendimentos passivos'
+    },
+    {
+        name: 'Bônus'
+    },
+    {
+        name: 'Investimento'
+    },
+    {
+        name: 'Empréstimo'
+    },
+    {
+        name: 'Férias'
+    },
+    {
+        name: '13º Sálario'
+    },
+    {
+        name: 'PLR'
+    },
+    {
+        name: 'Aposentadoria'
+    },
+    {
+        name: 'Aluguel'
+    },
+    {
+        name: 'Outros'
+    }
+];
+
+window.paymentMethod = [
+    {
+        name: 'Dinheiro'
+    },
+    {
+        name: 'Cheque'
+    },
+    {
+        name: 'Cartão de débito'
+    },
+    {
+        name: 'Cartão de crédito'
+    },
+    {
+        name: 'Pix'
+    },
+    {
+        name: 'Transferência bancária'
+    },
+    {
+        name: 'Ordem de Pagamento'
+    },
+    {
+        name: 'Boleto bancário'
+    },
+    {
+        name: 'Outros'
+    }
+];
+
 window.apiURL = 'http://localhost:3000';
